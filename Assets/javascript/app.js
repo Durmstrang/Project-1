36f99976260c098a7bc2be2bd93657f5d6451c18
Kayla Himmelberger <kaylahimmelberger@Edwards-MacBook-Pro.local>
2018-09-13 21:24:45 -0400

    updated js file with pseudocode

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,9 +1,15 @@
 // Psuedo code for our Parker App
 
-//Build search box that
+// Build search box as a splash page in the main index.html
+    // this should take advantage of the algolia JS library which will autofill what the user is typing
 
-//Grab users location and store input (variable)
+// Grab users location and store input (variable) in firebase
+    /* from firebase we can pull the state info and use Bing/Mapquest API to generate a map, then use the
+    National Park Service API to populate "pins" on that map of state parks in the user's state 
+        --our stretch goal is to change this to show all national parks within 50 miles from it */
 
-// use input to put in a format that both of our api's understand (NPS API & Leaflet) 
+/* Once the user click ons a "pin", show some handpicked data from the park's webpage 
+(Location, operating hours, entrance fee/pass information)
+    --stretch goal is to add photos, possibly from instagram and/or add weather info? */
 
-//toggle between a physical map and a dropdown list 
+// create a toggle menu in the top right of the page to show previous searches
