f83228aaf25e3090362cef6bf27c7f36281e1455
Lauren Santosuosso <Lauren.Santosuosso@yahoo.com>
2018-09-13 20:34:41 -0400

    some psuedo code for js

---
+++ Assets/javascript/app.js
@@ -0,0 +1,9 @@
+// Psuedo code for our Parker App
+
+//Build search box that
+
+//Grab users location and store input (variable)
+
+// use input to put in a format that both of our api's understand (NPS API & Leaflet) 
+
+//toggle between a physical map and a dropdown list 
