4fa6c172f0030f7cc31e591f4a3f88782270cc40
Kayla Himmelberger <38771553+kaylahimmel@users.noreply.github.com>
2018-09-15 13:57:46 -0400

    moved script from HTML to the JS

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,6 +1,27 @@
 // Psuedo code for our Parker App
-$(document).ready(function(){
 
+
+// MAPQUEST STUFF----------------------------------------------------------------------------------------------------
+window.onload = function() {
+    L.mapquest.key = 'q3aVXF4M4Hq6z0fi3Ithx6UFbnKa4aRIn45OIpKo';
+  
+    var lat = 37.7749
+    var long = -122.4194
+    
+    var map = L.mapquest.map('map', {
+        center: [lat, long],
+        layers: L.mapquest.tileLayer('map'),
+        zoom: 12
+    });
+
+    map.addControl(L.mapquest.control());
+}
+
+// CAPTURE DATA FROM SUBMIT BUTTON----------------------------------------------------------------------------------
+$("#submitBtn").on("click", function(event) {
+    event.preventDefault();
+    var locationInput = $("#location-input").val().trim()
+    console.log("location: " + locationInput)
 
 
 // Grab users location and store input (variable) in firebase
@@ -13,6 +34,3 @@ $(document).ready(function(){
     --stretch goal is to add photos, possibly from instagram and/or add weather info? */
 
 // create a toggle menu in the top right of the page to show previous searches
-
-})
-
