a568fb7f79ff65ebd2e649739724731dae7e7003
Zverloff <toddzverloff@gmail.com>
2018-09-22 14:45:58 -0400

    maybe done?

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,65 +1,145 @@
-// MAPQUEST STUFF----------------------------------------------------------------------------------------------------
-window.onload = function() {
-    L.mapquest.key = 'q3aVXF4M4Hq6z0fi3Ithx6UFbnKa4aRIn45OIpKo';
+var obj = {}
+     
+    function addMapBtn(park, targetDiv, parkName) {
+        var parkLat = ""
+        var parkLong = ""
+        if (park.latLong) {
+            var firstSplit = park.latLong.split(", ")
+            parkLat = firstSplit[0].replace("lat:", "")
+            parkLong = firstSplit[1].replace("long:", "")
+            $(targetDiv).append("<br><button data-lat="+parkLat+" data-long="+parkLong+" data-name="+parkName+" class='view-on-map' role='button' class='btn btn-success btn-lg'>VIEW ON MAP</a>")
+            
+        }
+            
+    }
+
+                    
+    var addMarker = function(e){
+                    map.remove()
+                    
+                    var mapDiv = $('<div id="map" style="width: 100%; height: 500px;">')
+                    var parkLat = e.target.attributes[0].nodeValue
+                    var parkLong = e.target.attributes[1].nodeValue
+                    var parkName = e.target.attributes[2].nodeValue
+                    
+                    $("#map-home").append(mapDiv)
+                    loadMap(parkLat, parkLong, parkName)
+    }
+    $("body").on("click", ".view-on-map", addMarker) 
+
+    var displayParks = function(para) {
+        var stateCode = para
+        var apiKey = "q3aVXF4M4Hq6z0fi3Ithx6UFbnKa4aRIn45OIpKo"
+        var queryURL = "http://api.nps.gov/api/v1/parks?stateCode=" + stateCode + "&fields=images" + "&api_key=" + apiKey
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function(response) {
+            window.obj.parks = response.data
+            createTable()
+
+            
+        })
+    }
+
+    function createTable() {
+        for (var i = 0; i < obj.parks.length; i++) {
+                
+
+            // adding the items from object to the table
+            var newRow = $("<tr class='clickable' data-toggle='collapse'>")
+            var newTr = $("<tr><td>")
+            var newTd = $("<td>")
+            var targetDiv = $("<div class='collapse'>")
+            var park = window.obj.parks[i]
+            var parkImage;
+            var parkDescription = park.description
+            var parkDirections = park.directionsInfo
+            var parkDirectionsURL = park.directionsUrl
+
+            $("#table-body").append(newRow).append(newTr)
+            $(newTr).append(newTd)
+            $(newTd).append(targetDiv)
+            if (park.images.length > 0 ) {
+                parkImage = park.images[0].url
+            $(targetDiv).append("<img class='park-image' src='" + parkImage + "'>")
+            }
+
+            $(targetDiv).append("<div id='park-description'><h4>Description: </h4><p>" + parkDescription + "</p></div>").append("<div id='park-directions'><h4>Directions: </h4><p>" + parkDirections + "</p></div>").append("<a href='" + parkDirectionsURL +"' role='button' class='btn btn-success btn-lg' target='_blank'>GET DIRECTIONS FROM NPS</a>")
+            // <a href='" + parkDirectionsURL + "' role='button' class='btn btn-success'></div>")
+            var newColumn = $("<th scope='col'>")
+            $(newRow).append(newColumn)
+            var newLink = $("<a>")
+            $(newColumn).append(newLink)
+            var parkLink = park.url
+            newLink.attr("href", parkLink)
+            var parkName = park.fullName
+            var parkCode = park.parkCode
+            $(newRow).attr("data-target","#"+ parkCode).append(newColumn2).append(newColumn3)
+            $(targetDiv).attr("id", parkCode)
+            $(newLink).append(parkName)
+            var newColumn2 = $("<th scope='col'>")
+            var parkDesignation = park.designation
+            $(newColumn2).append(parkDesignation)
+            var newColumn3 = $("<th scope='col'>")
+            var parkState = park.states
+            $(newColumn3).append(parkState)
+            // 
+            addMapBtn(park, targetDiv, parkName)
+        }
+    }
   
-    var lat = 44
-    var long = -94
     
-    var map = L.mapquest.map('map', {
-        center: [lat, long],
-        layers: L.mapquest.tileLayer('map'),
-        zoom: 12
-    });
-
-    map.addControl(L.mapquest.control());
-
-   
     
-        // This function handles events where the submit button is clicked
- $("#submit").on("click", function(event) {
-    event.preventDefault();
     
-   });
-}
-// var clickState = document.getElementById("simpleStateForm").value 
-// console.log(clickState)
-
-var al = $("#al").attr("value");
-console.log(al);
-
-
-
-
-
-  
-
-
-
-
-
-// }
-// ALGOLIA STUFF----------------------------------------------------------------------------------------------------
-var placesAutocomplete = places({
-    container: document.querySelector('#address-input')
-  });
-
-console.log("SOS");
-
-
-/* CAPTURE DATA FROM SUBMIT BUTTON----------------------------------------------------------------------------------
-$("#submitBtn").on("click", function(event) {
-    event.preventDefault();
-    var locationInput = $("#location-input").val().trim()
-    console.log("location: " + locationInput) */
-
-
-// Grab users location and store input (variable) in firebase
-    /*  1) use Algolia to autofill the city, state.
-        2) use Mapquest API to generate a map of the state. 
-        3) use the city, state info to run the state through the National Park Service API.
-        4) populate "pins" on the Mapquest map of state parks in the user's state 
-
-/* Once the user click ons a "pin", show some handpicked data from the park's webpage 
-(Location, operating hours, entrance fee/pass information)
-    --stretch goal: is to show all national parks within 50 miles instead of just in 
-    that state. Also add photos, possibly from instagram and/or add weather info? */
+    // HTML 5 geolocation code - yet to be implemented as of 9/19
+    var geoLocation = function() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            console.log("Geolocation is not supported by this browser")
+        }
+    }
+    
+    // Function for initializing mapquest map based on lat, long into the #map div
+    var loadMap = function(lat, long, name) {
+        L.mapquest.key = 'q3aVXF4M4Hq6z0fi3Ithx6UFbnKa4aRIn45OIpKo';
+        var map = L.mapquest.map('map', {
+            center: [lat, long],
+            layers: L.mapquest.tileLayer('map'),
+            zoom: 8
+        }); map.addControl(L.mapquest.control());
+            if (name) {
+            L.mapquest.textMarker([lat, long], {
+                text: name,
+                position: "right",
+                type: "marker",
+                icon: {
+                primaryColor: "#333333",
+                secondaryColor: "#333333",
+                size: "sm"
+            }
+            }).addTo(map)
+            }
+    }
+    
+    $("#submitBtn").on("click", function(event) {
+        event.preventDefault()
+        $("#table-body").empty()
+        var y = $("#stateSelection option:selected").val();
+        var stateLat =  $("#stateSelection option:selected").attr("data-lat")
+        var stateLong =  $("#stateSelection option:selected").attr("data-long")
+        // console.log(y)
+        // console.log(stateLat)
+        // console.log(stateLong)
+        displayParks(y)
+        map.remove()
+        var mapDiv = $('<div id="map" style="width: 100%; height: 500px;">')
+        $("#map-home").append(mapDiv)
+        loadMap(stateLat, stateLong)
+        })
+    
+    // Initial Load
+        
+        loadMap(41.26093905,-81.57116722)
+        
