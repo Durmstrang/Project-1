a1103354dbd1be6461cd84e0db16f2c20f78428b
Kayla Himmelberger <kaylahimmelberger@gmail.com>
2018-09-26 11:13:20 -0400

    attempted to add code to JS and HTML (also created a PHP file) to pass the API key to the JS without actually leaving it in the code since we are getting errors from NPS after publishing to GitHub Pages.

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -71,6 +71,20 @@ var displayParks = function(ST) {
     })
 }
 
+// <?php
+// // Get cURL resource
+// $curl = curl_init();
+// // Set options
+// $dataURL = 'https://developer.nps.gov/api/v1/parks?stateCode=me';
+// curl_setopt_array($curl, array(
+//     CURLOPT_RETURNTRANSFER =--> true,
+//     CURLOPT_URL => $dataURL,
+//     CURLOPT_USERAGENT => $_SERVER['HTTP_USER_AGENT'],
+//     CURLOPT_HTTPHEADER => array('Authorization: AHxYpzNIdier9Velo2UerDKo7wSmswmujiAOecJt')
+//     ));
+// // Additional code would follow
+// ?>
+
 // GET WEATHER DATA FROM OpenWeatherMaps API
 
 function getWeather(lat, lon, targetDiv) {
---
+++ Assets/php/nps_api_authenticate.php
@@ -0,0 +1,13 @@
+<?php
+// Get cURL resource
+$curl = curl_init();
+// Set options
+$dataURL = 'https://developer.nps.gov/api/v1/parks?stateCode=me';
+curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER =--> true,
+    CURLOPT_URL => $dataURL,
+    CURLOPT_USERAGENT => $_SERVER['HTTP_USER_AGENT'],
+    CURLOPT_HTTPHEADER => array('Authorization: AHxYpzNIdier9Velo2UerDKo7wSmswmujiAOecJt')
+    ));
+// Additional code would follow
+?>
